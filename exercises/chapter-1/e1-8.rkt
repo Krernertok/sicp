f7607b05ba320cc104f9c5e99c31f578da68fd55
Antti Sivula <antti.j.sivula@gmail.com>
2023-07-15 00:16:23 +0300

    Add solution for Exercise 1.8

---
+++ exercises/chapter-1/e1-8.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+       guess
+       (cbrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
+
+(define (square x) (* x x ))
+
+(define (cube x) (* x x x ))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+
+(cbrt 429130)
+
+(define answer (cbrt 429130))
+(cube answer)
